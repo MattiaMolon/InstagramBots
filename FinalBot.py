1611b3f8b6ccab5455bb6e48d13d628a2ade24e2
Mattia Molon <mmattia06@gmail,com>
2018-10-01 17:09:09 +0200

    added function get_hashtag_posts

--- FinalBot.py
+++ FinalBot.py
@@ -17,13 +17,15 @@ COMMENT_TIME = 20
 class InstagramBot:
 
     # inizializzazione
+    # username = username dell'utente
+    # password = password dell'utente
     def __init__ (self, username, password):
         self.username = username
         self.password = password
         #inoltre dobbiamo inizializzare anche il driver
         self.driver = webdriver.Firefox()
     
-    
+
     # effettuo login
     def login(self):
         
@@ -78,3 +80,28 @@ class InstagramBot:
             return True
         except NoSuchElementException:
             return False       
+
+
+    # prendi url delle foto da una pagina di ricerca di un hashtag, li restituisce in un vettore
+    # scrolls = numeri di scrolls da fare della pagina (33 link con 0 scrolls + 9 per scroll)
+    # hashtag = hashtag ricercato senza # all'inizio
+    def get_hashtag_posts(self, scrolls, hashtag):
+        driver = self.driver
+
+        # vado sull'hashag richiesto
+        driver.get("https://www.instagram.com/explore/tags/" + hashtag + "/")
+        time.sleep(DOWNLOAD_TIME)
+
+        # scorro la pagina 
+        for i in range(0, scrolls):
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(DOWNLOAD_TIME)
+
+        # prendo i riferimenti alle foto caricate
+        a_vector = driver.find_elements_by_tag_name("a")
+        pic_hrefs = [elem.get_attribute("href") for elem in a_vector]
+        pic_hrefs = [href for href in pic_hrefs if "?tagged=" + hashtag in href]
+
+        return pic_hrefs
+
+
